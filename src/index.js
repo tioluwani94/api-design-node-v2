ecca8dbe2771c5a3480462940ccea291c7914294
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2021-07-22 17:00:57 +0100

    complete exercise 1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import http from 'http'
-import { createServer } from 'http'
+import http from "http";
+import { createServer } from "http";
+import app from "./server";
 
-import app from './server'
-let currentApp = app
+let currentApp = app;
 
+app.listen(3001, () => {
+  console.log("http://localhost:3001");
+});
 
 if (module.hot) {
-	module.hot.accept(['./server'], () => {
-		server.removeListener('request', currentApp)
-		server.on('request', app)
-		currentApp = app
-	})
+  module.hot.accept(["./server"], () => {
+    server.removeListener("request", currentApp);
+    server.on("request", app);
+    currentApp = app;
+  });
 }
--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
-import express from 'express'
-import setupMiddware from './middleware'
-import { restRouter } from './api'
-import { connect } from './db'
-import { signin, protect } from './api/modules/auth'
+import express from "express";
+import setupMiddware from "./middleware";
+import { restRouter } from "./api";
+import { connect } from "./db";
+import { signin, protect } from "./api/modules/auth";
 // Declare an app from express
-const app = null
+const app = express();
 
-export default app
+app.get("/", (req, res) => {
+  res.json({ ok: true });
+});
+
+export default app;
